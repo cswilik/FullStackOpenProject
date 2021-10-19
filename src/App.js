15e4da5a55feb4d4fdfda1f3ca0cf5ee1d61227f
CSWILIK <cswilik@gmail.com>
2021-10-19 17:47:16 -0400

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello name="Chelsey" age='33'/>
+      <Hello name="Jimmy" age='31'/>
     </div>
   );
+}
+
+function Hello(props) {
+
+  return (<div className="App">
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>)
 }
 
 export default App;
